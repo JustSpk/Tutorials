9be924c1020b8ae378ac890e1f595b6b62d60f5c
Pradeep Kumar S <kumarkumar0058@gmail.com>
2019-12-23 17:17:12 +0530

    Practicing Servlets with Session

--- ServletWrk/src/com/spk/servletwrk/ServletWrk.java
+++ ServletWrk/src/com/spk/servletwrk/ServletWrk.java
@@ -8,17 +8,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ServletWrk extends HttpServlet {
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
 		int i=Integer.parseInt(req.getParameter("num1"));
 		int j=Integer.parseInt(req.getParameter("num2"));
 		int k=i+j;
+		
 		PrintWriter out=res.getWriter();
-		out.println("k ="+k);
-		req.setAttribute("k", k);
-		RequestDispatcher rd=req.getRequestDispatcher("sq");
-		rd.forward(req, res);
+		
+		HttpSession session=req.getSession();
+		session.setAttribute("k", k);
+		
+		res.sendRedirect("sq");
+//		req.setAttribute("k", k);
+//		
+//		RequestDispatcher rd=req.getRequestDispatcher("sq");
+//		rd.forward(req, res);
 	}
 	
 }
